e1a0502e33baac592b42e95d4328522f765d8b6a
Colin Yang Hong <colinyang@nmg.com.hk>
2013-09-25 10:25:23 +0800

    Initial Commit

---
+++ Do_StoryBoard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Do_StoryBoard
+//
+//  Created by Colin Yang Hong on 13-9-25.
+//  Copyright (c) 2013年 Yang Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
